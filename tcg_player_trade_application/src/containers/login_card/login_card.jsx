665f13b4ab79a49623bb3c6dfdda35c979328b25
Reece Cristea <reece.cristea@mymail.champlain.edu>
2024-02-07 13:36:05 -0800

    finished login card animation and hide/show password, still need to do css to make it look prettier, working on sign up card after that

--- tcg_player_trade_application/src/containers/login_card/login_card.jsx
+++ tcg_player_trade_application/src/containers/login_card/login_card.jsx
@@ -1,22 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login_card.css'
 import 'animate.css'
 
-const LoginCard = ({setLoginPage}) => {
+const LoginCard = ({ setLoginPage }) => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className='login-card-content animate__animated animate__slideInUp'>
+    <div id="login" className='login-card-content animate__animated animate__zoomIn'>
       <label>Email</label>
-      <input className='login-input' type='text' placeholder='Enter your email'></input>
+      <input
+        className='login-input'
+        type="email"
+        id="email"
+        pattern=".+@example\.com"
+        size="30"
+        required
+        placeholder='Enter your email'
+        onChange={(e) =>
+          setEmail(e.target.value)
+        }
+      />
       <label>Password</label>
-      <input className='login-input' type='text' placeholder='Enter your password'></input>
+      <input
+        id="pass"
+        className='login-input'
+        type={
+          showPassword ? "text" : "password"
+        }
+        value={password}
+        onChange={(e) =>
+          setPassword(e.target.value)
+        }
+        placeholder='Enter your password'
+      />
+      <label>Show Password</label>
+      <input
+        id="check"
+        type="checkbox"
+        className='checkbox-container'
+        value={showPassword}
+        onChange={() =>
+          setShowPassword((prev) => !prev)
+        }
+      />
       <div className='login-interactables'>
         <button className='login-card-button'>Login</button>
         <hr></hr>
         <p className='sign-up-text'>Don't have an account yet? <button onClick={() => {
-        setLoginPage(false);
-      }}>Sign Up!</button></p>
+          console.log(email);
+          console.log(password);
+          document.getElementById("login").classList.remove('animate__zoomIn');
+          document.getElementById("login").classList.add('animate__zoomOut');
+          console.log(document.getElementById("login").classList)
+          setTimeout(() => {
+            setLoginPage(false);
+          }, 500);
+          
+        }}>Sign Up!</button></p>
       </div>
-      
+
     </div>
   )
 }
