6a43a9adb45451defbe3b847e21d57c0da05612f
Reece Cristea <reece.cristea@mymail.champlain.edu>
2024-02-11 09:36:08 -0800

    added some html structure and data for display, still need to style with css and add other sellers at bottom of page

--- tcg_player_trade_application/src/pages/CardPage/CardPage.jsx
+++ tcg_player_trade_application/src/pages/CardPage/CardPage.jsx
@@ -1,31 +1,65 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './CardPage.css'
 import { useParams } from 'react-router-dom';
 import Axios from 'axios';
-import  { Navbar } from '../../containers'
-
-
-const fetchCard = async (card_id) => {
-    const apiPath = "http://localhost:3001/getSelectedCardData";
-    try {
-        const res = await Axios.get(apiPath, { params: {
-            cardId: card_id
-        } });
-        console.log(res.data);
-        return res.data;
-    } catch (err) {
-        console.log("Error: " + err);
-    }
-}   
+import { Navbar } from '../../containers'
 
 const CardPage = () => {
+    const fetchCard = async (card_id) => {
+        const apiPath = "http://localhost:3001/getSelectedCardData";
+        try {
+            const res = await Axios.get(apiPath, {
+                params: {
+                    cardId: card_id
+                }
+            });
+            console.log(res.data);
+            return res.data;
+        } catch (err) {
+            console.log("Error: " + err);
+        }
+    }
+    
     const cardParam = useParams();
-    const cardData = fetchCard(cardParam.card_id);
+    const [cardData, setCardData] = useState([]);
+    
+    const [cardDisplayData, setCardDisplayData] = useState({});
+
+    useEffect(() => {
+        const fetchCard = async (card_id) => {
+            const apiPath = "http://localhost:3001/getSelectedCardData";
+            try {
+                const res = await Axios.get(apiPath, {
+                    params: {
+                        cardId: card_id
+                    }
+                });
+                setCardData(res.data);
+                setCardDisplayData(res.data[0])
+            } catch (err) {
+                console.log("Error: " + err);
+            }
+        }
+        fetchCard(cardParam.card_id);
+    }, [])
 
     return (
 
         <div>
             <Navbar />
+            <div className='card-page-container'>
+                <div className='card-page-img-container'>
+                    <img className='card-page-img' src={cardDisplayData.card_picture_url} alt={cardDisplayData.card_name}/>
+                </div>
+                <div className='card-page-content'>
+                    <h2>Card Name: {cardDisplayData.card_name}</h2>
+                    <h2>Category: {cardDisplayData.category_name}</h2>
+                    <h2>Card Condition: {cardDisplayData.individual_card_condition}</h2>
+                    <h2>Card Price: {cardDisplayData.individual_card_price}</h2>
+                    <h2>Quantity: {cardDisplayData.individual_card_quantity}</h2>
+                    <h2>Card Owner: {cardDisplayData.uname}</h2>
+                </div>
+            </div>
         </div>
     )
 }
